9b7477b89b9ce21f01905190be6f3a9f2e2020d0
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-03-03 11:05:06 +0900

    map set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'stations' => 'data#stations'
+    get 'ways' => 'data#ways'
+    get 'teams' => 'data#teams'
+    get '' => 'home#index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/data_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DataControllerTest < ActionDispatch::IntegrationTest
+  test "should get stations" do
+    get data_stations_url
+    assert_response :success
+  end
+
+end
